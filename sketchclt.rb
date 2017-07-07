8d88eccde169c7bd0115187989a9fd66736ac7ef
Ivkin <ivkin@uf430b911398a59306065.ant.amazon.com>
2017-07-06 17:18:12 -0700

    version field added

--- sketchclt.rb
+++ sketchclt.rb
@@ -6,6 +6,7 @@ class Sketchclt < Formula
   desc ""
   homepage ""
   url "https://github.com/nikitaivkin/sketchCLT/archive/v0.0.1.tar.gz"
+  version "0.0.1"
   sha256 "1b992b6c66236835f3799479cd6b00329685c761dd2616a36424e9d24db7451d"
 
   def install
