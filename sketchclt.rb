ec25e7ed63e65af3da78a8e7446de13d06bcf7d2
Ivkin <ivkin@uf430b911398a59306065.ant.amazon.com>
2017-08-03 14:06:35 -0700

    updated version, added commons cli lib

--- sketchclt.rb
+++ sketchclt.rb
@@ -1,18 +1,19 @@
-# Homebrew formula for installation from the Homebrew Tap 
 # Documentation: http://docs.brew.sh/Formula-Cookbook.html
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Sketchclt < Formula
-  desc "sketch Uniques, Quantiles, Histograms, or Frequent Items"
-  homepage "http://datasketches.github.io"
-  url "https://github.com/nikitaivkin/sketchCLT/archive/v0.0.2.tar.gz"
-  sha256 "e70b7b8c605de451f6a7db6f570d3beb79d979be9862cdb10f5ecc93fa0a7184"
-  version "0.0.2"
+  desc ""
+  homepage ""
+  url "https://github.com/nikitaivkin/sketchCLT/archive/v0.0.3.tar.gz"
+  sha256 "1cf093c3c648f6e1058d7c4b2803862cbc15294bdf538183040cfb631e219275"
+  version "0.0.3"
 
   def install
-    inreplace "sketch", "##PREFIX##", "#{prefix}"    # putting correct paths into bash script
-    prefix.install "sketches-core-0.8.4.jar","memory-0.8.4.jar","sketches-misc-0.8.3-SNAPSHOT.jar"  #copying libs to prefix
-    bin.install "sketch" #copying sketch script to bin folder
+    inreplace "sketch", "##PREFIX##", "#{prefix}"
+    prefix.install "sketches-core-0.8.4.jar", "commons-cli-1.4.jar","memory-0.8.4.jar","sketches-misc-0.8.3-SNAPSHOT.jar"
+    bin.install "sketch"
   end
 
 end
+  
